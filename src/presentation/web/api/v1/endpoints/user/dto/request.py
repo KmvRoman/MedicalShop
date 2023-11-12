5f5151c18609edc476376beed89f96e507047cc0
KmvRoman <iswift977@gmail.com>
2023-11-12 17:34:10 +0500

    put max length for string inputs

--- src/presentation/web/api/v1/endpoints/user/dto/request.py
+++ src/presentation/web/api/v1/endpoints/user/dto/request.py
@@ -1,6 +1,6 @@
 from datetime import date, datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from src.domain.product.entities.product import ProductId
 from src.domain.user.write.entities.client import ClientId
@@ -8,17 +8,17 @@ from src.domain.user.write.entities.employee import EmployeeId
 
 
 class CreateClientRequest(BaseModel):
-    full_name: str
+    full_name: str = Field(max_length=100)
     birthdate: date
 
 
 class CreateEmployeeRequest(BaseModel):
-    full_name: str
+    full_name: str = Field(max_length=100)
     birthdate: date
 
 
 class CreateProductRequest(BaseModel):
-    name: str
+    name: str = Field(max_length=255)
     quantity: int
     price: int
 
